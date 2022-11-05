14fc7048023d50c25d5788ffc0a9f1c0eaa616be
DanilVelikiy <danil.velikiy@gmail.com>
2022-11-05 19:51:01 +0300

    v.2 Переделал под Бизорд шаблон, стали приятнее по стилю. Добавил еще один проект в портфолио - по разработке стратегии участия в конкурсах. Надо сделать админку, очень неудобно заполнять контент

--- projects/models.py
+++ projects/models.py
@@ -6,13 +6,41 @@ class Project(models.Model):
     technology = models.CharField(max_length=20)
     image = models.FilePathField(path="/img")
     target = models.TextField(default=None, null=True)
-    target = models.TextField(default=None , null=True)
     block_1_text = models.TextField(default=None, null=True)
     block_1_image = models.FilePathField(path="/img", default=None, null=True)
     block_2_text = models.TextField(default=None , null=True)
     block_2_image = models.FilePathField(path="/img" , default=None , null=True)
     block_3_text = models.TextField(default=None , null=True)
     block_3_image = models.FilePathField(path="/img" , default=None , null=True)
-#    paragraph_txt_1 = models.TextField(default=None, null=True)
-#    paragraph_img_1 = models.FilePathField(path="/img" , default=None , null=True)
+
+    paragraph_txt_1 = models.TextField(default=None, null=True)
+    paragraph_img_1 = models.FilePathField(path="/img" , default=None , null=True)
+
+    paragraph_txt_2 = models.TextField(default=None , null=True)
+    paragraph_img_2 = models.FilePathField(path="/img" , default=None , null=True)
+
+    paragraph_txt_3 = models.TextField(default=None , null=True)
+    paragraph_img_3 = models.FilePathField(path="/img" , default=None , null=True)
+
+    paragraph_txt_4 = models.TextField(default=None , null=True)
+    paragraph_img_4 = models.FilePathField(path="/img" , default=None , null=True)
+
+    paragraph_txt_5 = models.TextField(default=None , null=True)
+    paragraph_img_5 = models.FilePathField(path="/img" , default=None , null=True)
+
+    paragraph_txt_6 = models.TextField(default=None , null=True)
+    paragraph_img_6 = models.FilePathField(path="/img" , default=None , null=True)
+
+    paragraph_txt_7 = models.TextField(default=None , null=True)
+    paragraph_img_7 = models.FilePathField(path="/img" , default=None , null=True)
+
+    paragraph_txt_8 = models.TextField(default=None , null=True)
+    paragraph_img_8 = models.FilePathField(path="/img" , default=None , null=True)
+
+    paragraph_txt_9 = models.TextField(default=None , null=True)
+    paragraph_img_9 = models.FilePathField(path="/img" , default=None , null=True)
+
+    paragraph_txt_10 = models.TextField(default=None , null=True)
+    paragraph_img_10 = models.FilePathField(path="/img" , default=None , null=True)
+
     # сылку на гитхаб надо еще
